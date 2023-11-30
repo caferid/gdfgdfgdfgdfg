0f34c5138da4b403442f6c54c45cf5743eaf302d
Iszdn <tu7k21xbe@code.edu.az>
2023-11-30 13:13:17 +0400

    4cu gun

--- src/components/Feature__items/index.jsx
+++ src/components/Feature__items/index.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Feature_item from './Feature_item'
+import React from "react";
+import Feature_item from "./Feature_item";
+import useFetch from "../../hooks/useFetch";
 
 function Feature__items() {
+  const baseUrl = 'http://localhost:3000/features'
+  const {data} = useFetch(baseUrl)
   return (
     <>
-    <div className="container">
-        <div className="row ">
-                <Feature_item/>
-                <Feature_item/>
-                <Feature_item/>
+      <section id="feature-items" style={{backgroundColor:"black"}}>
+        <div className="container">
+          <div className="row ">
+            {data && data.map(item => (
+
+            <Feature_item key={item.id} {...item}/>
+            ))}
+
+          </div>
         </div>
-    </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Feature__items
+export default Feature__items;
